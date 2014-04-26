a97528d67b21344c02601da62e12a13625b4ad20
Sylvain Juge <syl20j@gmail.com>
2014-04-26 18:44:41 +0200

    wip

--- src/test/java/sylvain/juge/inventory/IndexTest.java
+++ src/test/java/sylvain/juge/inventory/IndexTest.java
@@ -2,31 +2,53 @@ package sylvain.juge.inventory;
 
 
 import com.google.common.hash.Hashing;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class IndexTest {
 
-    @SuppressWarnings("deprecation")
-    private final sylvain.juge.inventory.util.TempFolder tempFolder = sylvain.juge.inventory.util.TempFolder.createNew();
+    private static final String EMPTY_FILE_SHA1 = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
 
-    @AfterTest
-    private void after(){
-        tempFolder.cleanup();
+    @Test
+    public void emptyIndex() throws IOException {
+
+        try (FileSystem fs = newMemoryFs()) {
+
+            Path root = fs.getPath("/");
+
+            Index index = Index.newIndex(root, Hashing.sha1());
+            assertThat(index.getEntries()).isEmpty();
+            assertThat(index.getRoot()).isEqualTo(root);
+
+        }
     }
 
     @Test
-    public void emptyIndex(){
+    public void emptyFileIndex() throws IOException {
+        try (FileSystem fs = newMemoryFs()) {
+            Path root = fs.getPath("/");
+            Files.createFile(root.resolve("file"));
 
-        Path root = tempFolder.getPath();
+            Index index = Index.newIndex(root, Hashing.sha1());
+            index.update();
+            assertThat(index.getEntries()).hasSize(1);
 
-        Index index = Index.newIndex(root, Hashing.sha1());
-        assertThat(index.getEntries()).isEmpty();
-        assertThat(index.getRoot()).isEqualTo(root);
+            IndexEntry entry = index.getEntries().get(0);
+            assertThat(entry.getSize()).isEqualTo(0);
+            assertThat(entry.getHash()).isEqualTo(EMPTY_FILE_SHA1);
+        }
+    }
+
+    private FileSystem newMemoryFs() throws IOException {
+        return FileSystems.newFileSystem(URI.create("memory:/test"), null);
     }
 
     // create index with specific items
